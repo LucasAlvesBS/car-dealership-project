86398f005aaffce176e33d6265358d0b04eccd6c
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-12 12:59:45 -0300

    feat(typeorm): add typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { CustomersModule } from './app/customers/customers.module';
 import { VehiclesModule } from './app/vehicles/vehicles.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({}), CustomersModule, VehiclesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    } as TypeOrmModuleOptions),
+    CustomersModule,
+    VehiclesModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/app/vehicles/vehicles.module.ts
+++ src/app/vehicles/vehicles.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vehicles } from 'src/shared/entities/vehicle.entity';
 import { VehiclesController } from './vehicles.controller';
 import { VehiclesService } from './vehicles.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vehicles])],
   controllers: [VehiclesController],
-  providers: [VehiclesService]
+  providers: [VehiclesService],
+  exports: [VehiclesService],
 })
 export class VehiclesModule {}
