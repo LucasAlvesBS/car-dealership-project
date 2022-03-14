e70631dcdb75b9e6ac841a6e18e4867d23e37d8b
Lucas Alves <lucasalvesbs6@gmail.com>
2022-03-14 15:34:11 -0300

    feat(typeorm): add debuf property

--- ormconfig.js
+++ ormconfig.js
@@ -5,11 +5,15 @@ module.exports = {
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
-  ssl: {
-    rejectUnauthorized: false,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
-  entities: ['dist/shared/entities/*.entity.js'],
-  migrations: ['dist/configs/typeorm/migrations/*.js'],
+  debug: true,
+  entities: [process.env.ENTITIES],
+  migrations: [process.env.MIGRATIONS],
   cli: {
     entitiesDir: 'src/shared/entities/',
     migrationsDir: 'src/configs/typeorm/migrations',
--- src/app.module.ts
+++ src/app.module.ts
@@ -12,10 +12,14 @@ import { OrdersModule } from './app/orders/orders.module';
       type: process.env.DB_TYPE,
       url: process.env.DATABASE_URL,
       synchronize: false,
-      logging: false,
-      ssl: {
-        rejectUnauthorized: false,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
+      debug: true,
+      logging: false,
       entities: [__dirname + '/**/*.entity{.js,.ts}'],
     } as TypeOrmModuleOptions),
     CustomersModule,
